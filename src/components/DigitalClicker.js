a67b95b7df97fc5a2ed482f76028e4c220892d04
owenabbott <ocabbott@gmail.com>
2020-06-08 10:00:23 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    state = {
+        timesClicked: 0
+    }
+
+    // handleClick = () => {
+    //     this.setState({
+    //         timesClicked: this.state.timesClicked + 1
+    //     })
+    // }
+    //While it works, the above is bad practice apparently, due to async issues. Below is the correct way of handling updates.
+
+    handleClick = () => {
+        this.setState( prevState => {
+            return {
+                timesClicked: prevState.timesClicked + 1
+            }
+        });
+    }
+
+    render() {
+        return (
+            <button onClick={()=>this.handleClick()}>{this.state.timesClicked}</button>
+        )
+    }
+}
